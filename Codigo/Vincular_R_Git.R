1eb50d73d1494886f8af061a3c6f464fcc2549ec
Yoel7576 <7576425921@untrm.edu.pe>
2025-05-28 20:39:08 -0500

    SE

--- Codigo/Vincular_R_Git.R
+++ Codigo/Vincular_R_Git.R
@@ -6,8 +6,17 @@
 system('git config --global user.name "Henry"')
 system('git config --global user.email "henrysantillanculquimboz@gmail.com"')
 
-# verificar
+## verificar
 system('git config --global --list')
 
+
+
+# desde el git Bash
+
+git config --global user.name "Tu Nombre"
+git config --global user.email "tucorreo@example.com"
+
+## verificar
+git config --global --list
 
 
